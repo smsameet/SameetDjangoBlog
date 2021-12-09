710288cae8596cf5b0a84f191302e4563c37f0f5
sameet <wpf_sam@yahoo.com>
2021-12-09 22:30:07 +0330

    create home function ( home view ).

--- SameetBlog/blog/views.py
+++ SameetBlog/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    ''' this is function of the blog home. '''
+    context = {}
+    return render(request, 'home/base.html', context)
