b3bcd94776a34ca57259c15dfb645829feee4e17
sameet <wpf_sam@yahoo.com>
2022-03-02 09:01:56 +0330

    switching to mariadb ( database ).

--- SameetBlog/settings.py
+++ SameetBlog/settings.py
@@ -106,12 +106,15 @@ WSGI_APPLICATION = 'SameetBlog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'USER': 'test',
+    'NAME': 'text',
+    'PASSWORD': 'test',
+    'HOST': 'test',
+    'PORT': '101010',
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
